c1d99effdb2704b7a0274842b2588b252dee0b84
gabriel <gabrielzero2bi@gmail.com>
2025-04-03 14:31:00 -0600

    El programa ya programa!

--- App.js
+++ App.js
@@ -6,48 +6,45 @@ export default function App() {
   return (
     <View style={styles.container}>
       <View style={styles.contenedorLogo}>
-      <Image style={styles.logo} source={require('./assets/Imagenes/Logo.png')}/>
-      
+        <Image style={styles.logo} source={require('./assets/Imagenes/Logo.png')}/>
       </View>
+
       <Text style={styles.titulo}> Esto es lo que nuestros alumnos dicen sobre freeCodeCamp: </Text>
 
- <ScrollView style={styles.scrol}>  
-      <View style={styles.contenedorTestimonio}>
-        <View style={styles.contenedorImagen}>
-           <Testimonio
-            imagenP = {require('./assets/Imagenes/chino.png')}/>   
-            <Testimonio           
-          imagenP = {require('./assets/Imagenes/negra.png')}/>  
-          <Testimonio           
-          imagenP = {require('./assets/Imagenes/chela.png')}/>  
-           </View>
-           
+     <ScrollView style={styles.scrol}>  
+    
+     Trazabilidad del café 
+Implementación de la trazabilidad en la cadena de valor del café 
+Inteligencia artificial aplicada a la trazabilidad
+Buenas prácticas agrícolas
+Comercio justo y certificaciones de la industria
 
-        <View style={styles.contenedorTexto}>
-          <Testimonio
+
+   <Testimonio
+           imagenP = {require('./assets/Imagenes/chino.png')}
            ciudad = 'Wang Shawn en Singapur'
            cargo = 'Ingeniero de Software en Amazonas'
            testimonio = '"Es aterrador cambiar de carrera. Solo gané la confianza de que podía codificar trabajando a través de las cientos de horas de lecciones gratuitas en freeCodeCamp. En un año tuve un trabajo de seis cifras como Ingeniero de Software. freeCodeCamp cambió mi vida."'
-          />
+    />
 
-          <Testimonio
+    <Testimonio
+           imagenP = {require('./assets/Imagenes/negra.png')}
            ciudad = 'Sarah Chima en Nigeria'
            cargo = 'Ingeniero de Software en ChatDesk'
            testimonio = '"freeCodeCamp fue la puerta de entrada a mi carrera como desarrollador de software. El plan de estudios bien estructurado llevó mi conocimiento de codificación de un nivel de principiante total a un nivel muy seguro. Era todo lo que necesitaba para conseguir mi primer trabajo de desarrollo en una empresa increíble."'
-          />
 
-          <Testimonio
-           ciudad = 'Emma Bostiana en Suecia'
-           cargo = 'Ingeniero de Software en Spotify'
-           testimonio = '"Siempre he luchado por aprender JavaScript. He tomado muchos cursos, pero el curso de freeCodeCamp fue el que se quedó. Estudiar JavaScript, así como estructuras de datos y algoritmos en freeCodeCamp me dio las habilidades y la confianza que necesitaba para conseguir el trabajo de mis sueños como ingeniero de software en Spotify."'
-          />
+    />
 
-           </View>
+    <Testimonio
+          imagenP = {require('./assets/Imagenes/chela.png')}
+          ciudad = 'Emma Bostiana en Suecia'
+          cargo = 'Ingeniero de Software en Spotify'
+          testimonio = '"Siempre he luchado por aprender JavaScript. He tomado muchos cursos, pero el curso de freeCodeCamp fue el que se quedó. Estudiar JavaScript, así como estructuras de datos y algoritmos en freeCodeCamp me dio las habilidades y la confianza que necesitaba para conseguir el trabajo de mis sueños como ingeniero de software en Spotify."'
+
+    />
 
 
-
-        </View> 
-        </ScrollView>
+     </ScrollView>
       <StatusBar style="auto" />
     </View>
   );
@@ -57,41 +54,26 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+    
   },
   scrol: {
-width:'100%'
+    width:'100%'
   },
   contenedorLogo: {
     height:80,
     width: '100%',
     alignItems: 'center',
-    justifyContent: 'center'
-
+    justifyContent: 'center',
+    marginBottom: 20,
   },
   logo: {
     height:80,
     width: "100%"
-  },
-  contenedorTestimonio:{
-    flex: 1,
-    flexDirection: 'row',
-    minHeight: 250,
-    backgroundColor: '#fff'
   },
   titulo: {
     fontSize: 20,
     fontWeight: 'bold',
     textAlign: 'center'
   },
-  contenedorImagen: {
-flex: 1,
-justifyContent: 'flex-start'
-
-  },
-
-  contenedorTexto: {
-    flex: 2,
-    justifyContent: 'flex-start'
-     },
 });
 
--- Componentes/Testimonio.js
+++ Componentes/Testimonio.js
@@ -2,12 +2,16 @@ import {View, Text, StyleSheet, Image} from "react-native";
 
 function Contexto(props){
     return(
-        <View>
-            <Text style={styles.ciudad}>{props.ciudad}</Text>
-            <Text style={styles.cargo}>{props.cargo}</Text>
-            <Text style={styles.testimonio}>{props.testimonio}</Text>
-            <Image style={styles.imagenP} source={props.imagenP}/>
-                
+        <View style={styles.conteiner}>
+            <View style={styles.conteinerImagen}>
+              <Image style={styles.imagenP} source={props.imagenP}/>
+            </View>
+
+            <View style={styles.contenedorTestimonio}>
+              <Text style={styles.ciudad}>{props.ciudad}</Text>
+              <Text style={styles.cargo}>{props.cargo}</Text>
+              <Text style={styles.testimonio}>{props.testimonio}</Text>
+            </View>
         </View>
 
     );
@@ -18,28 +22,51 @@ function Contexto(props){
 export default Contexto;
 
 const styles = StyleSheet.create({
+    conteiner:{
+        flex: 1,
+        flexDirection: 'row',
+        minHeight:250,
+        backgroundColor: '#fff',
+        marginTop:15,
+
+    },
+    imagenP:{
+        height:250,
+        width: '100%'
+
+    },
     ciudad:{
         fontSize: 12,
+        marginTop: 20,
         color: 'black',
-         alignSelf: 'center'
-
+        alignSelf: 'center'
+        
     },
     cargo:{
         fontSize: 12,
+        marginBottom: 5,
         color: 'black',
          alignSelf: 'center'
     },
     testimonio:{
         fontSize: 12,
         color: 'black',
-        alignSelf: 'center'
-        
+        alignSelf: 'center',
     },
     imagenP:{
         height: 250,
         width: '100%',
+        
+    },
+    conteinerImagen: {
+        flex: 1,
         alignSelf: 'center'
-    }
+        
+    },
+    contenedorTestimonio: {
+        flex: 2,
+        alignItems: 'center'
+   },
 
 
 })
